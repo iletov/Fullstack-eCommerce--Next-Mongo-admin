62df91d179133633da7619f2a070efc9d13bb360
Ivan Letov <letov88@gmail.com>
2023-05-09 01:25:05 +0300

    Connecting to DB

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,25 @@
-import Nav from "@/components/Nav"
-import { useSession, signIn, signOut } from "next-auth/react"
+import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
-  const { data: session } = useSession()
-
-  if(!session) {
-
-   return (
-    <div className='bg-blue-900 w-screen h-screen flex items-center'>
-      <div className='text-center w-full'>
-        <button 
-          className='bg-white p-2 px-4 rounded-lg'
-          onClick={() => signIn('google')}
-          >Login with Google</button>
-      </div>
-    </div>
-   ) 
-  }
+  const {data: session} = useSession();
+  console.log({session})
   
-  return (
-    <div className="bg-blue-900 min-h-screen flex">
-      <Nav />
-      <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">
-        Signed in as {session.user.email}
+  return ( 
+    <Layout>
+      <div className="text-blue-900 flex justify-between">
+        <h2>
+          <b>Hello, {session?.user?.name}</b>
+        </h2>
+        <div className="flex bg-gray-300 text-black gap-1 rounded-lg">
+          <img src={session?.user?.image} alt='' className="w-8 h-8 overflow-hidden" />
+          <span className="px-2">
+            {session?.user?.name}
+          </span>
+          
+        </div>
+        
       </div>
-    </div>
-    
+    </Layout>
   )
 }
